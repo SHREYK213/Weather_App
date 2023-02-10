7000beadab0dcf2259aeedc567cf8fdb928ef055
SHREYK213 <shreyaspoojary240@gmail.com>
2023-02-10 21:26:19 +0530

    Weather_app final

---
+++ app.js
@@ -0,0 +1,34 @@
+const posStack = require('./utils/posStack')
+const forecast = require('./utils/forecast')
+
+
+
+const address = process.argv[2]
+
+
+if (!address) {
+    console.log('Please enter a location')
+} else {
+
+    posStack(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+
+            return console.log(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+
+            if (error) {
+                return console.log(error)
+            }
+            console.log(location)
+            console.log(forecastData)
+
+        })
+    })
+}
+
+
+
+
+
